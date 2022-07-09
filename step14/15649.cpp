9f17ec121b898ff0a7717c02c1b7abfa4df7039d
TenJeong <33208278+TenJeong@users.noreply.github.com>
2022-07-09 20:23:21 +0900

    15649번 코드 수정

--- step14/15649.cpp
+++ step14/15649.cpp
@@ -4,25 +4,24 @@
 using namespace std;
 
 int n, m;
-vector<int> myStack;
-int arr[9] = {0};
+int arr[9] = {0}; // 선택한 숫자들
+int visited[9] = {0}; // 앞에서 선택 여부를 저장 
 
 void find(int count){
+    if(count == m){ // 다 선택 했을 시 출력
+        for(int i = 0; i < m; i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return ;
+    }
+
     for(int i = 1; i <= n; i++){
-        if(arr[i] == 0){
-            myStack.push_back(i);
-            arr[i] = 1;
-
-            if(count - 1 == 0){
-                for(int v : myStack){
-                    cout<<v<<' ';
-                }
-                cout<<'\n';
-            }
-            else find(count-1);
-
-            myStack.pop_back();
-            arr[i] = 0;
+        if(visited[i] == 0){    // 방문한적이 없다면
+            visited[i] = 1;     // 방문 체크 후 저장
+            arr[count] = i;
+            find(count+1);  // count번째 이후를 선택함
+            visited[i] = 0; // count 이후를 다 선택 했으면 count번째를 초기화
         }
     }
 }
@@ -35,7 +34,7 @@ int main(){
 
     cin>>n>>m;
 
-    find(m);
+    find(0);
 
     return 0;
 }
