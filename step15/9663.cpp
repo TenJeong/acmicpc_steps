ce145743623df3de177fbffd7030c425d1995f3a
TenJeong <tenj845@gmail.com>
2022-07-12 19:46:37 +0900

    9663번 코드 수정

--- step15/9663.cpp
+++ step15/9663.cpp
@@ -7,23 +7,25 @@ int n;
 int case_count = 0;
 int arr[16] = {0};
 
+bool check(int qnum){
+    int current = arr[qnum];
+    for(int i = 1; i < qnum; i++){  // 이전 행들의 퀸과 비교
+        if(arr[i] == current || (qnum - i) == abs(current - arr[i]))    // 같은 열에 있거나 대각선에 존재 할 경우...
+            return false;
+    }
+    return true;
+}
+
 void nqueens(int qnum){
     if(qnum == n + 1){ // n개를 모두 선택했을 경우 count
         case_count++;
         return ;
     }
 
-    for(int i = 1; i <= n; i++){    // qnum 번째 열의 퀸 위치 선정
-        bool fittable = true;   // i번째 행에 퀸을 놓을 수 있는가?
-        for(int j = 1; j < qnum; j++){  // 이전 행들의 퀸과 비교
-            if(arr[j] == i || abs(qnum - j) == abs(i - arr[j])){    // 같은 열에 있거나 대각선에 존재 할 경우...
-                fittable = false;   // i에 놓지 못함
-                break;
-            }
-        }
-        if(!fittable) continue;
-        arr[qnum] = i;  // 놓을 수 있다면 i 열를 선택하고 다음 행의 퀸을 선택함
-        nqueens(qnum + 1);
+    for(int i = 1; i <= n; i++){    // qnum번째 열의 퀸 위치 선정
+        arr[qnum] = i;  // qnum의 i열을 선택
+        if(check(qnum))    // qnum행의 i열에 퀸을 놓을 수 있는가?
+            nqueens(qnum+1);   // 놓을 수 있다면 다음 행의 퀸을 선택함
     }
 }
 
