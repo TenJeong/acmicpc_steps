58662db88d4c5157f35e5c72e7ae6e0ea7fd16d7
TenJeong <tenj845@gmail.com>
2022-07-16 23:08:07 +0900

    24416번 코드 수정

--- step16/24416.cpp
+++ step16/24416.cpp
@@ -2,41 +2,23 @@
 
 using namespace std;
 
-int fib_count = 0;
-int fibonacci_count = 0;
-
-int fib(int n){
-    if(n == 1 || n == 2) {
-        fib_count++;    // 코드 1 카운트
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
-}
-
-int fibonacci(int n){
-    int f[41] = {0};
-
-    f[1] = f[2] = 1;
-
-    for(int i = 3; i <= n; i++){
-        fibonacci_count++;  // 코드 2 카운트
-        f[i] = f[i - 1] + f[i - 2];
-    }
-
-    return f[n];
-}
-
 int main(){
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     int n;
+    int f[41] = {0};
     cin>>n;
+    
+    f[1] = f[2] = 1;
+    for(int i = 3; i <= n; i++){
+        f[i] = f[i - 1] + f[i - 2];
+    }
 
-    fib(n);
-    fibonacci(n);
+    // 코드1은 return 1의 횟수, 즉 1의 개수이므로 f[N]과 같다.
+    // 코드2는 f[1]과f[2]를 제외한 수, 즉 N-2와 같다.
 
-    cout<<fib_count<<' '<<fibonacci_count;
+    cout<<f[n]<<' '<<n-2;
 
     return 0;
 }
