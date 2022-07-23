860aa59a9adaf4a2e4383064c4d1de187bcd2e78
TenJeong <tenj845@gmail.com>
2022-07-23 21:56:04 +0900

    2579번 코드 수정

--- step16/2579.cpp
+++ step16/2579.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int best_score[301][2];  // i번쨰까지 최대 비용, [i번째 계단][i-1에서 경로, i-2에서 경로]
-int step[301][2]; // +1의 횟수, 2가 되면 +1 불가능
+int best_score[301];
 int score[301];
 
 int main(){
@@ -19,34 +18,16 @@ int main(){
         cin>>score[i];
     }
 
-    best_score[0][0] = best_score[0][1] = 0;
-    step[0][0] = step[0][1] = 0;
+    best_score[0] = 0;
+    best_score[1] = score[1];
+    best_score[2] = max(best_score[0], best_score[1]) + score[2];
 
-    for(int i = 0; i < n; i++){
-        // i-1을 이용하여 i+1로의 경로 계산 
-        if(i + 1 <= n && step[i][0] < 2 && best_score[i+1][0] < best_score[i][0] + score[i+1]){
-            step[i+1][0] = step[i][0] + 1;
-            best_score[i+1][0] = best_score[i][0] + score[i+1];
-        }
-        // i-2를 이용하여 i+1로의 경로 계산
-        if(i + 1 <= n && step[i][1] < 2 && best_score[i+1][0] < best_score[i][1] + score[i+1]){
-            step[i+1][0] = step[i][1] + 1;
-            best_score[i+1][0] = best_score[i][1] + score[i+1];
-        }
-
-        // i-1을 이용하여 i+2로의 경로 계산
-        if(i + 2 <= n && best_score[i+2][1] < best_score[i][0] + score[i+2]){
-            step[i+2][1] = 1;
-            best_score[i+2][1] = best_score[i][0] + score[i+2];
-        }
-        // i-2를 이용하여 i+2로의 경로 계산
-        if(i + 2 <= n && best_score[i+2][1] < best_score[i][1] + score[i+2]){
-            step[i+2][1] = 1;
-            best_score[i+2][1] = best_score[i][1] + score[i+2];
-        }
+    for(int i = 3; i <= n; i++){
+        // i-2에서 오거나 i-3에서 i-1을 거쳐서 오는 경우 2가지 비교
+        best_score[i] = max(best_score[i-2], best_score[i-3] + score[i-1]) + score[i];
     }
 
-    cout<<max(best_score[n][0],  best_score[n][1]);
+    cout<<best_score[n];
 
     return 0;
 }
