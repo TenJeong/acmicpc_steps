0ab3e10bfcc901873b55a7da949bbf5a13d6d114
TenJeong <33208278+TenJeong@users.noreply.github.com>
2022-07-26 19:27:13 +0900

    2156번 코드 수정

--- step16/2156.cpp
+++ step16/2156.cpp
@@ -20,15 +20,17 @@ int main(){
 
     // dp[i]는 i보다 작거나 같은 것들 중에서 마지막에 i를 마셨을 때의 최대값
     amount[0] = dp[0] = 0;
-    dp[1] = amount[1]; dp[2] = amount[1]+amount[2];
+    dp[1] = amount[1]; dp[2] = amount[1] + amount[2];
 
-    for(int i = 3; i <= n; i++){
-        // dp[i]는 dp[i-2], dp[i-3] + amount[i-1], dp[i-4] + amount[i-1]의 값 중에서 큰 값에 i의 값을 더한 것과 같다.
-        dp[i] = max(dp[i-2], max(dp[i-3] + amount[i-1], dp[i-4] + amount[i-1])) + amount[i];
+    dp[3] = max(amount[1] + amount[3], max(amount[2] + amount[3], amount[1] + amount[2]));
+
+    for(int i = 4; i <= n; i++){
+        // 1. i를 마시고 i-1를 마실 경우 2. i를 마시고 i-1를 마시지 않을 경우 3. i를 마시지 않을 경우
+        dp[i] = max(dp[i-3] + amount[i-1] + amount[i], max(dp[i-2] + amount[i], dp[i-1]));
     }
 
     // 최대값을 찾아서 출력한다.
-    cout<<max(dp[n],max(dp[n-1], dp[n-2]));
+    cout<<dp[n];
 
     return 0;
 }
