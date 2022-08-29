019f59d24eb805c018033a8fc7e23c35c3cd30c6
TenJeong <tenj845@gmail.com>
2022-08-29 23:49:57 +0900

    10830번 코드 수정

--- step21/10830.cpp
+++ step21/10830.cpp
@@ -5,28 +5,27 @@ using namespace std;
 int n;
 long long b;
 
-int originalMat[5][5]; // 초기 배열
+long long originalMat[5][5]; // 초기 배열
 long long matA[5][5]; // 행렬곱의 결과가 저장됨
-long long matB[5][5]; // 행렬곱을 위한 임시 행렬
 
-void matMul(){
+void matMul(long long matX[5][5], long long matY[5][5]){
     long long matMulResult[5][5];
 
-    // matA와 matB의 곱을 구해서 matMulResult라는 행렬에 저장
+    // matX와 matY의 곱을 구해서 matMulResult라는 행렬에 저장
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             matMulResult[i][j] = 0;
             for(int k = 0; k < n; k++){
-                matMulResult[i][j] += matA[i][k] % 1000 * matB[k][j] % 1000;
+                matMulResult[i][j] += matX[i][k] % 1000 * matY[k][j] % 1000;
                 matMulResult[i][j] %= 1000;
             }
         }
     }
 
-    // 행렬곱의 결과를 matA로 옮김
+    // 행렬곱의 결과를 matX로 옮김
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            matA[i][j] = matMulResult[i][j];
+            matX[i][j] = matMulResult[i][j];
         }
     }
 }
@@ -48,38 +47,13 @@ void dnc(long long counts){
 
     // counts가 홀수 -> mat^(counts/2) * mat^(counts/2) * mat
     if(counts % 2 == 1){
-        // matB에 mat^(counts/2)를 복사
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matB[i][j] = matA[i][j];
-            }
-        }
 
-        // 행렬곱 수행
-        matMul();
-
-        // matB에 초기 행렬 복사
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matB[i][j] = originalMat[i][j];
-            }
-        }
-
-        // 행렬곱 수행
-        matMul();
+        matMul(matA, matA);
+        matMul(matA, originalMat);
     }
     // counts가 짝수 -> mat^(counts/2) * mat^(counts/2)
     else{
-
-        // matB에 mat^(counts/2)를 복사
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matB[i][j] = matA[i][j];
-            }
-        }
-
-        // 행렬곱 수행
-        matMul();
+        matMul(matA, matA);
     }
 }
 
